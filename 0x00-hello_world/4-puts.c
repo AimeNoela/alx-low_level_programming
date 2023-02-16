6ca0f83339229da1028b9c6b4f2dd2e510a66cb4
AimeNoela <aimeenoella@gmail.com>
2023-02-16 02:44:52 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Entry point to print a line with put function
+ * Return: Always 0 (success)
+ * */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
