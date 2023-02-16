8723ae3f0dbac8c02c4952bf1f621c53a111e499
AimeNoela <aimeenoella@gmail.com>
2023-02-16 04:03:34 -0800

    4puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - Entry point to print a line with put function
  * Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
