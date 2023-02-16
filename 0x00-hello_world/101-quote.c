c85009227f523f05805a49620c3d621051ecbf59
AimeNoela <aimeenoella@gmail.com>
2023-02-16 06:23:14 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a c program thaprint a quote to the standard error
+ * Return: 1(Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
