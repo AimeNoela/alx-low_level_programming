803c14c44973179cc1d5f43216f8c025ce67ff8b
AimeNoela <aimeenoella@gmail.com>
2023-02-21 08:37:50 -0800

    main.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - main bloc
+ * Description:  print _putchar with a new linw
+ * Retrun: 0
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	retun(0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write c
+ *
+ * Return: 1 (Success)
+ */
+int _putchar(char c)
+{
+	Return (write(1, &c, 1))
+}
