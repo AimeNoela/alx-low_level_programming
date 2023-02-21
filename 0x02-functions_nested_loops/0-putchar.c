2d8e20eda394dd92b97d2c1aade2cdd9f75a6a8e
AimeNoela <aimeenoella@gmail.com>
2023-02-21 11:14:51 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
+
 /**
  * main - main block
  * Description:  print _putchar with a new linw
@@ -8,14 +7,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_pitchar('_');
+	_pitchar('p');
+	_pitchar('u');
+	_pitchar('t');
+	_pitchar('c');
+	_pitchar('h');
+	_pitchar('a');
+	_pitchar('r');
+	_pitchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-_pitchar(char c);
+int _pitchar(char c);
 
 #endif
