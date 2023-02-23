e420ce3f61b4793dd7e4ff36d949ee0c8229133e
AimeNoela <aimeenoella@gmail.com>
2023-02-23 04:00:20 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,18 +8,18 @@
 
 void print_most_numbers(void)
 {
-	int n = 0;
+	int x = 0;
 
-	for (; n <= 9; n++)
+	for (; x <= 9; x++)
 	{
-		if (n == 2 || n == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(n + '0');
-		}
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
 	}
 	_putchar('\n');
 }
