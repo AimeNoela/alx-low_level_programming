6f8311fad5724d37eb559fa1b7315b7bf4cbda7d
AimeNoela <aimeenoella@gmail.com>
2023-02-27 05:33:53 -0800

    task5

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts2 -print every other character of a string
+ * starting with the first character
+ * @str: input
+ * Return: printed number
+ */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int a;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+
+	for (a = 0; a <= t; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
+}
