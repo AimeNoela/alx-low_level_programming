30cb99bbba2a0914a283de508a63dfa8df0cab13
AimeNoela <aimeenoella@gmail.com>
2023-03-02 10:47:58 -0800

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,6 +2,7 @@
 
 /**
  * infinite_add - adds two numbers
+ * rev_string: output
  * @n1: variable or numberfirst to compute
  * @n2: variable or second number to compute
  * @r: the buffer
@@ -28,7 +29,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + 1) - '0';
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else
@@ -38,7 +39,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			overflow = 1;
 		else
 			overflow = 0;
-		if (digits >= (sie_r - 1))
+		if (digits >= (size_r - 1))
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
