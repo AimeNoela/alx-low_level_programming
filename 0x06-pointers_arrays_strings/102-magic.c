95c69224884ae1186758fa712032196eaf291b87
AimeNoela <aimeenoella@gmail.com>
2023-03-02 13:58:30 -0800

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -18,11 +17,11 @@ int main(void)
 /*
  * write your line of code ...
  * main - magic
- * You are not allowed to use the variable a in your new line of code
- * You are not allowed to modify the variable p
- * You can only write one statement
- * You are not allowed to use ,
- * You are not allowed to code anything else than the line
+ * -You are not allowed to use the variable a in your new line of code
+ * -You are not allowed to modify the variable p
+ * -You can only write one statement
+ * -You are not allowed to use ,
+ * -You are not allowed to code anything else than the line
  * of expected line of code at the expected line
  */
 
