652771bb5d010f809079a5f85e4ae8d97c8737b6
AimeNoela <aimeenoella@gmail.com>
2023-03-08 07:20:42 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the lengh of a string
+ * @s: the measured string
+ * Return: the lngh of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		lent++;
+		ln += _strlen_recurs(s + 1);
+	}
+}
