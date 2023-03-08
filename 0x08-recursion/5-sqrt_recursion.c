28477642959b44e937c7a3a4b09fcff7eefa1deb
AimeNoela <aimeenoella@gmail.com>
2023-03-08 13:21:16 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,44 +1,34 @@
 #include "main.h"
 
 /**
- * _evaluate - evaluate the integer
- * @n: the integer
- * @i: the integer
- * Return: natural square root of n
- * or -1 if the number does not have natural square root
+ * _sqrt_helper - Returns the natural square root of a number.
+ * @n: The number to find the square root of.
+ * @i: The current number being checked.
+ *
+ * Return: The natural square root of @n or -1 if @n does not have a
+ *         natural square root.
  */
 
-int _evaluate(int i, int n)
+int _sqrt_helper(int n, int i)
 {
-	if (n == 0 || n == 1)
-		return (n);
-
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-
-	else if (i * i == n)
+	if (i * i == n)
 		return (i);
-
-	return (-1);
-
-	return (-1);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt_helper(n, i + 1));
 }
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: the integer
- * @i: the integer
- * Return: Sgrt_recursion
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to find the square root of.
+ *
+ * Return: The natural square root of @n or -1 if @n does not have a
+ *         natural square root.
  */
 
 int _sqrt_recursion(int n)
 {
-	int i = 0;
-
-	if (i < 0)
-	{
+	if (n < 0)
 		return (-1);
-	}
-	else if (i >= 0)
-		return (_evaluate(i, n));
+	return (_sqrt_helper(n, 0));
 }
