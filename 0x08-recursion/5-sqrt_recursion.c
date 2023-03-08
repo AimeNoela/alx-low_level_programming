ca40063408825804b217575e3d5535dc8735d495
AimeNoela <aimeenoella@gmail.com>
2023-03-08 09:41:36 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _evaluate - evaluate the integer
+ * @n: the integer
+ * @i: the integer
+ * Return: natural square root of n
+ * or -1 if the number does not have natural square root
+ */
+
+int _evaluate(int i, int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+
+	else if (i * i < n)
+		return (_evaluate(i + 1, n));
+
+	else if (i * i == n)
+		return (i);
+
+	return (-1);
+
+	return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the integer
+ * @i: the integer
+ * Return: Sgrt_recursion
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+	{
+		return (_evaluate(i, n));
+	}
+}
