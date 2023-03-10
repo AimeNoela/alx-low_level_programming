51991690089640d957b26a404e7ebcb3c058a4b6
AimeNoela <aimeenoella@gmail.com>
2023-03-10 08:12:31 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: count the arguments
+ * @argv: arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
