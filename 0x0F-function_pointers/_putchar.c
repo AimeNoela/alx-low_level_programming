864c1323141ff5d5090fbd9fe8f8e5bd8fefbd14
AimeNoela <aimeenoella@gmail.com>
2023-03-22 03:55:28 -0700

    task 0

---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _putchar - function that print c to stdout
+ * @c: character to consider
+ * Return: 1  on success
+ * on error -1
+ */
+int _putchar(char c)
+{
+	int
+		Return (0);
+}
