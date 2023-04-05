c05c08b1aebcf6f5b129ce703daf3cb73e533704
AimeNoela <aimeenoella@gmail.com>
2023-04-05 13:50:35 -0700

    task 0 updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,16 +3,16 @@
 
 /**
  * print_list -  prints all the elements of a list_t list
- * @h: the list_t list to consider
+ * @h: the list_t lis considered
  *
- * Return: the number of nodes in h
+ * Return: the number of nodes available in h
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	for (; h; h = h->next)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -21,7 +21,6 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		nodes++;
-		h = h->next;
 	}
 	return (nodes);
 }
