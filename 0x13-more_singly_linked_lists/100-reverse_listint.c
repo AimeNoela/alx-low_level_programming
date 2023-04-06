3b208e3aee7eebfad519f7855b403c116a407812
AimeNoela <aimeenoella@gmail.com>
2023-04-05 18:30:36 -0700

    update

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,12 +11,11 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prev = NULL;
 	listint_t *next = NULL;
 
-	while (*head)
+	for (; *head; *head = next)
 	{
 		next = (*head)->next;
 		(*head)->next = prev;
 		prev = *head;
-		*head = next;
 	}
 	*head = prev;
 
