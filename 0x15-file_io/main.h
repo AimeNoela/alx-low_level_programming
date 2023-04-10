b77b52c121a97c68e344d849cbe307b377d861a3
AimeNoela <aimeenoella@gmail.com>
2023-04-10 15:43:23 -0700

    task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,11 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+int cp(char *file_from, char *file_to);
+void print_usage_error(void);
+void print_read_error(char *filename);
+void print_write_error(char *filename);
+void print_close_error(int fd);
+
 
 #endif
