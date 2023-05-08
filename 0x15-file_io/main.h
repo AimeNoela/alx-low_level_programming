1b676a2af62bb6a562d15c1c264faed948c0e585
Your Name <you@example.com>
2023-05-08 04:58:20 -0700

    advanced

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -19,6 +19,15 @@ void print_read_error(char *filename);
 void print_write_error(char *filename);
 void print_close_error(int fd);
 int elf_header(const char *filename);
-
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
